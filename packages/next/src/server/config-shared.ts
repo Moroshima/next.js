a5231119864cdd84c7a45703f4c7a64801f18022
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-08-27 10:57:02 +0200

    Add `experimental.appNewScrollHandler`

--- packages/next/src/server/config-shared.ts
+++ packages/next/src/server/config-shared.ts
@@ -441,6 +441,7 @@ export interface LoggingConfig {
 
 export interface ExperimentalConfig {
   adapterPath?: string
+  appNewScrollHandler?: boolean
   useSkewCookie?: boolean
   cacheHandlers?: {
     default?: string
@@ -1470,6 +1471,7 @@ export const defaultConfig = Object.freeze({
   allowedDevOrigins: undefined,
   experimental: {
     adapterPath: process.env.NEXT_ADAPTER_PATH || undefined,
+    appNewScrollHandler: false,
     useSkewCookie: false,
     cacheLife: {
       default: {
